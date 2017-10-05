e88d46f5ff9a73ce106140da90c1362a93a49804
Michał Siatkowski <msiatkowski@pl.imshealth.com>
2017-10-05 18:07:49 +0200

    add casperjs tests

---
+++ test/test.js
@@ -0,0 +1,32 @@
+var phantomcss = require('phantomcss');
+
+casper.test.begin('LogosArt', function () {
+
+    phantomcss.init({
+        rebase: casper.cli.get("rebase"),
+        casper: casper,
+        libraryRoot: '',
+        screenshotRoot: './test/screenshots/',
+        addLabelToFailedImage: false
+    });
+
+    casper.options.waitTimeout = 20000;
+
+    casper.start('http://localhost:4000/', function() {
+        this.waitForSelector('img.map-img');
+    });
+    casper.viewport(1920, 1080);
+
+    casper.wait(1000, function () {
+        phantomcss.screenshot('html');
+    });
+
+    casper.then(function () {
+        phantomcss.compareAll();
+    });
+
+    casper.run(function () {
+        phantomcss.getExitStatus(); // pass or fail?
+        casper.test.done();
+    });
+});
